6f8e5f5c9edd01923d4431fbfa0f888f0a59020c
Alan Gaurav <alangaurav@gmail.com>
2024-03-11 00:59:45 +0000

    community: Models: Minor updates
    Both tag and post models now include categories.
    
    Signed-off-by: Alan Gaurav <alangaurav@gmail.com>

--- community/models.py
+++ community/models.py
@@ -21,6 +21,15 @@ class Profile(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
+    categories = [
+        ('location', 'location'),
+        ('company', 'company'),
+        ('restaurant', 'restaurant'),
+        ('accomodation', 'accomodation'),
+        ('advice', 'advice'),
+        ('resource', 'resources')
+    ]
+    category = models.CharField(max_length=15, choices=categories)
 
 class Post(models.Model):
     created_on = models.DateTimeField(default=timezone.now, editable=False)
@@ -30,9 +39,16 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     tags = models.ManyToManyField(Tag)
     location = models.TextField(blank=True)
+    categories = [
+        ('request', 'request'),
+        ('recommendation', 'recommendation'),
+        ('post', 'post')
+    ]
+    category = models.CharField(max_length=15, choices=categories)
 
 class Comment(models.Model):
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     created_on = models.DateTimeField(default=timezone.now, editable=False)
     description = models.TextField()
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='tags', blank=True, null=True)
