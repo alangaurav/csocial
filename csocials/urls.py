8eabbbd3842427ecf3ea6ca9f2e166d4528e36bd
Alan Gaurav <alangaurav@gmail.com>
2024-03-14 14:40:31 +0000

    csocials: urls: Add Media urls
    Signed-off-by: Alan Gaurav <alangaurav@gmail.com>

--- csocials/urls.py
+++ csocials/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from community import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('', include('community.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
